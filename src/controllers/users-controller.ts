06c564004efb99ed267c0f97cdf939af49e5cc76
Davi Lorena <davilorena2016@gmail.com>
2025-09-25 08:37:50 -0300

    feat: adiciona arquivo de controller e de rota para users

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+
+class UsersController {
+
+
+async create(req: Request, res: Response) {
+
+
+    res.json({message: "OK!"})
+}
+
+
+}
+
+export { UsersController };
