0389c51bac2ff67e866979acb982b10dff754501
Davi Lorena <davilorena2016@gmail.com>
2025-10-09 14:58:51 -0300

    feat: adiciona metodo para criar times

---
+++ prisma/migrations/20251009175253_optional_team_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."teams" ALTER COLUMN "description" DROP NOT NULL;
--- src/controllers/teams-controller.ts
+++ src/controllers/teams-controller.ts
@@ -1,12 +1,35 @@
 import { Request, Response } from "express"
 import { z } from "zod"
+import { prisma } from "@/database/prisma"
 import { AppError } from "@/utils/AppError"
 
 
 class TeamsController {
 
 async create(req: Request, res: Response) {
-    res.json({message: "OK"})
+
+    const bodySchema = z.object({
+        name: z.string().min(5, "Mínimo 5 caractertes").max(100, "Máximo 100 caracteres").trim(),
+        description: z.string().trim().optional()
+    })
+
+    const {name, description} = bodySchema.parse(req.body)
+
+    const nameExist = await prisma.teams.findFirst({where: {name}})
+
+    if(nameExist) {
+        throw new AppError("This name already exist!")
+    }
+
+ await prisma.teams.create({
+    data: {
+        name,
+        description
+    }
+ })
+
+
+    res.status(201).json({name, description})
 }
 
 
